2e7cec690a70ef80f9df28be4cbe5120aa7137e6
javier <velezj@alum.mit.edu>
2013-12-09 14:52:20 -0500

    adding plotting API for hte server (thin layer around erf-couchdb)

---
+++ src/plot.cpp
@@ -0,0 +1,164 @@
+
+#include "plot.hpp"
+#include <erf-couchdb/couchdb.hpp>
+
+
+using namespace boost::property_tree;
+using namespace couchdb;
+using namespace std;
+
+
+namespace plot_server {
+  namespace api {
+
+
+    namespace internal {
+
+      //=============================================================
+
+      static Couchdb _global_couchdb( "localhost:5984/plots-database/" );
+      
+      //=============================================================
+      
+      void open_globaldb( const boost::network::uri::uri& url )
+      {
+	_global_couchdb = Couchdb( url );
+      }
+
+      //=============================================================
+
+      Couchdb& globaldb()
+      {
+	return _global_couchdb;
+      }
+
+      //=============================================================
+
+      ptree
+      fetch_plot( const string& id ) 
+      {
+	return globaldb().fetch( id );
+      }
+      
+      //=============================================================
+
+      ptree
+      fetch_plot_sequence( const string& id )
+      {
+	return globaldb().fetch( id );
+      }
+      
+      //=============================================================
+
+      ptree
+      fetch_data_series( const string& id )
+      {
+	return globaldb().fetch( id );
+      }
+      
+      //=============================================================
+      
+
+    }
+
+    //=============================================================
+
+    string
+    add_data_series( const vector<data_point_t>& data ,
+		     const ptree& series_config )
+    {
+      ptree doc;
+      for( auto dpoint : data ) {
+	doc.add_child( "data_series.data", dpoint.attributes );
+      }
+      doc.put_child( "config", series_config );
+      ptree res = globaldb.save( doc );
+      return res.get<string>(id);
+    }
+
+
+    //=============================================================
+
+    void add_data_series_to_plot( const string& data_series_id,
+				  const string& plot_id )
+    {
+      ptree plot_doc = internal::fetch_plot( plot_id );
+      plot_doc.add( "plot.data_series.id", data_series_id );
+      ptree res = globaldb.save( plot_doc, plot_id );
+    }
+
+
+    //=============================================================
+
+    void add_plot_to_plot_sequence( const string& plot_id,
+				    const string& plot_sequence_id )
+    {
+      ptree seq_doc = internal::fetch_plot_sequence( plot_sequence_id );
+      seq_doc.add( "plot_sequence.plots.id", plot_id );
+      ptree res = globaldb.save( seq_doc, plot_sequence_id );
+    }
+
+    //=============================================================
+
+    string
+    create_plot( const ptree& plot_config,
+		 const vector<string>& data_series )
+    {
+      ptree plot_doc;
+      plot_doc.put_child( "config", plot_config );
+      ptree res = globaldb.save( plot_doc );
+      string id = res.get<string>("id");
+      for( string series_id : data_series ) {
+	add_data_series_to_plot( series_id, id );
+      }
+      return id;
+    }
+
+
+    //=============================================================
+
+    string
+    create_plot_sequence( const ptree& sequence_config,
+			  const vector<string>& plots )
+    {
+      ptree seq_doc;
+      seq_doc.put_child( "config", sequence_config );
+      ptree res = globaldb.save( seq_doc );
+      string id = res.get<string>( "id" );
+      for( string plot_id : plots ) {
+	add_plot_to_plot_sequence( plot_id, id );
+      }
+      return id;
+    }
+    
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    //=============================================================
+    
+
+
+
+  }
+}
---
+++ src/plot.hpp
@@ -0,0 +1,144 @@
+
+#if !defined( __PLOT_SERVER_API_plot_HPP__ )
+#define __PLOT_SERVER_API_plot_HPP__
+
+#include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/lexical_cast.hpp>
+#include <map>
+
+namespace plot_server {
+  namespace api {
+
+
+    //===============================================================
+
+    // Description:
+    // A 'data' point for plots.
+    // This is a 3D point plus a mapping of string attributes by name.
+    struct data_point_t
+    {
+    public:
+      boost::property_tree::ptree attributes;
+
+      data_point_t( const double& x, const double& y, const double& z ) 
+      {
+	set( "x", x );
+	set( "y", y );
+	set( "z", z );
+      }
+      data_point_t( const boost::property_tree::ptree& data )
+      {
+	attributes = data;
+      }
+
+      std::string operator() ( const std::string& at ) const {
+	return attributes.get<std::string>( at );
+      }
+      template< class T >
+      data_point_t& put( const std::string& at, const T& val ) {
+	attributes.put( at, val );
+      }
+      template<class T>
+      T get( const std::string& at ) const
+      {
+	return attributes.get<T>( at );
+      }
+      template< class T >
+      T get( const std::string& at, const T& default_val )
+      {
+	return attributes.get<T>( at, default_val );
+      }
+      
+    };
+
+    //===============================================================
+
+    // Description:
+    // adds a data series and returns it's id
+    std::string
+    add_data_series( const std::vector<data_point_t>& data ,
+		     const boost::property_tree::ptree& series_config );
+
+
+    //===============================================================
+
+    // Description:
+    // Creates a new plot and returns it's ID
+    // The plot will include the given data series
+    std::string
+    create_plot( const boost::property_tree::ptree& plot_config,
+		 const std::vector<std::string>& data_series );
+
+
+    //===============================================================
+
+    // Description:
+    // add a dat aseries to a plot
+    void add_data_series_to_plot( const std::string& data_series_id,
+				  const std::string& plot_id );
+
+    //===============================================================
+
+    // Description:
+    // Creates a new plot sequence and return it's ID
+    // Also adds the given plots to the sequence in order
+    std::string
+    create_plot_sequence( const boost::property_tree::ptree& sequence_config,
+			  const std::vector<std::string>& plots );
+
+    //===============================================================
+
+    // Description:
+    // Add a plot to a plot sequence
+    void add_plot_to_plot_sequence( const std::string& plot_sequence_id,
+				    const std::string& plot_id );
+    
+    //===============================================================
+
+
+    // Description:
+    // Internal API
+    namespace internal {
+
+      
+      // Description:
+      // Returns the entire structure for a plot given it's id
+      boost::property_tree::ptree
+      fetch_plot( const std::string& id );
+      
+      //===============================================================
+      
+      // Description:
+      // Returns the enture strucgure for a plot sequence given it's id
+      boost::property_tree::ptree
+      fetch_plot_sequence( const std::string& id );
+      
+      //===============================================================
+      
+      // Description:
+      // Returns the entire structure for a data series given it's id
+      boost::property_tree::ptree
+      fetch_data_series( const std::string& id );
+
+    }
+
+    //===============================================================
+    //===============================================================
+    //===============================================================
+    //===============================================================
+    //===============================================================
+    //===============================================================
+    //===============================================================
+    //===============================================================
+    //===============================================================
+    //===============================================================
+
+    
+
+  }
+}
+
+
+#endif
+
